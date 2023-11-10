034c373ecc8b33aed07491ca76b098369314594e
Sergey <sergey@MacBook-Pro-Sergey.local>
2023-11-10 17:20:12 +0100

    Initial commit

---
+++ src/HomeWorkLesson2.java
@@ -0,0 +1,20 @@
+public class HomeWorkLesson2 {
+    public static void main(String arg1[]) {
+        char c = 'G';
+        int i = 67;
+        byte bt = 67;
+        short s = 89;
+        float fl = 6.7888888f;
+        double d = 6.5456;
+        long l = 2345567;
+        boolean bl = true;
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(bt);
+        System.out.println(s);
+        System.out.println(fl);
+        System.out.println(d);
+        System.out.println(l);
+        System.out.println(bl);
+    }
+}
